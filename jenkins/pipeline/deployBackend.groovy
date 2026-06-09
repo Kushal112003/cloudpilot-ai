3a6ddb5f0c8a8f85a572866defa44eec0455df51
Ubuntu <ubuntu@ip-172-31-21-212.ap-south-1.compute.internal>
2026-06-09 07:24:17 +0000

    error correction deployBackend

--- jenkins/pipeline/deployBackend.groovy
+++ jenkins/pipeline/deployBackend.groovy
@@ -1,22 +1,21 @@
 echo "Deploying Backend to Kubernetes..."
 
 sh '''
-echo "Importing image into K3s"
+export KUBECONFIG=/var/lib/jenkins/.kube/config
+
+echo "Importing image into K3s..."
 
 docker save cloudpilot-backend:latest -o cloudpilot-backend.tar
 
 sudo k3s ctr images import cloudpilot-backend.tar
 
-echo "Applying Kubernetes manifests..."
+echo "Applying manifests..."
 
 kubectl apply -f kubernetes/namespace.yaml
-
 kubectl apply -f kubernetes/backend-deployment.yaml
-
 kubectl apply -f kubernetes/backend-service.yaml
 
 echo "Waiting for rollout..."
 
-kubectl rollout status deployment/cloudpilot-backend \ -n cloudpilot --timeout=120s
-
-''' 
+kubectl rollout status deployment/cloudpilot-backend -n cloudpilot --timeout=120s
+'''
