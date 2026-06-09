afa2cbc156106d749704bbb395e606886308b838
Ubuntu <ubuntu@ip-172-31-21-212.ap-south-1.compute.internal>
2026-06-09 07:29:34 +0000

    error correction healthCheckBackend

--- jenkins/pipeline/healthCheckBackend.groovy
+++ jenkins/pipeline/healthCheckBackend.groovy
@@ -3,8 +3,10 @@ echo "Running Kubernetes Health Check..."
 sh '''
 export KUBECONFIG=/var/lib/jenkins/.kube/config
 
-kubectl rollout status deployment/cloudpilot-backend \ -n cloudpilot --timeout=120s
+kubectl rollout status deployment/cloudpilot-backend \
+-n cloudpilot --timeout=120s
 
 kubectl get pods -n cloudpilot
 
 kubectl get svc -n cloudpilot
+'''
